c947b6859c240e0bb0a8eb971355897a8bf9372a
Diego Almeida <dyhalmeida@gmail.com>
2020-08-19 08:16:33 -0300

    Adicionado API do youtube

---
+++ src/api.js
@@ -0,0 +1,2 @@
+const API_KEY = 'AIzaSyBRI2e2_TjK1r3dCUA-pLhIvMUVcEc1Z5s';
+export default API_KEY;
--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import { Segment, Input } from 'semantic-ui-react';
 
 class SearchBar extends Component {
+
+    searchWord = e => {
+        const KEY_ENTER = 13;
+        if (e.keyCode === KEY_ENTER) {
+            const word = e.target.value;
+            console.log(word);
+        }
+    }
+
     render() {
         return (
             <div className="search-bar">
                 <Segment raised>
-                    <Input type="text" placeholder="Search..." size="large" icon="search" />
+                    <Input 
+                        type="text" 
+                        placeholder="Search..." 
+                        size="large" 
+                        icon="search"
+                        onKeyDown={(e) => this.searchWord(e)}
+                    />
                 </Segment>
             </div>
         );
