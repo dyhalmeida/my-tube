f71a92c2d7bb83064c2ba8f3ec6d1cf05350a91e
Diego Almeida <dyhalmeida@gmail.com>
2020-08-29 17:58:27 -0300

    Carregando videos da store com redux

--- src/components/VideoList/index.js
+++ src/components/VideoList/index.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { List, Image } from 'semantic-ui-react';
+import { connect } from 'react-redux';
 
-const VideoList = props => {
-    return (
-        <div className="video-list">
-            <h1>VideoList</h1>
-        </div>
-    )
+class VideoList extends Component {
+
+    renderVideo(video) {
+        console.log(video)
+        return (
+            <List.Item key={video.id.videoId}>
+            <Image src={video.snippet.thumbnails.default.url} />
+                <List.Content>
+                <List.Header>{video.snippet.title}</List.Header>
+                </List.Content>
+            </List.Item>
+        );
+    }
+
+    render() {
+        return (
+            <div className="video-list">
+                <List animated verticalAlign="middle">
+                   {this.props.videoList.map(video => this.renderVideo(video))}
+                </List>
+            </div>
+        )
+    }
 }
 
-export default VideoList;
+const mapStateToProps = (state) => {
+    const { videoList, loading, error } = state.videoSearch;
+    return {
+        videoList,
+        loading,
+        error
+    }
+}
+
+export default connect(mapStateToProps, null)(VideoList);
