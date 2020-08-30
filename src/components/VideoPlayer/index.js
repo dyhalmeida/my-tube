7e437c05bb6973df575ce78c56dd4938dea134b2
Diego Almeida <dyhalmeida@gmail.com>
2020-08-30 12:21:30 -0300

    Adicionado vídeo no embed player

--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Advertisement, Embed } from 'semantic-ui-react';
 
-const VideoPlayer = props => {
-    return (
-        <div className="video-player">
-            <h1>VideoPlayer</h1>
-        </div>
-    )
+class VideoPlayer extends Component {
+    render() {
+        return (
+            <div className="video-player">
+                {
+                    !this.props.video.id && (
+                        <Advertisement 
+                        style={{ 'height': '433px' }}
+                        unit="top banner"
+                        test="Escolha um vídeo para redroduzir"
+                    />
+                    )
+                }
+                {
+                    this.props.video.id && (
+                        <div>
+                            <Embed 
+                                id={this.props.video.id.videoId} 
+                                source="youtube"
+                            />
+                            <p className="video-player-title">{this.props.video.snippet.title}</p>
+                            <p className="video-player-description">{this.props.video.snippet.description}</p>
+                        </div>
+                    )
+                }
+               
+            </div>
+        )
+    }
 }
 
-export default VideoPlayer;
+const mapStateToProps = (state) => {
+    const { video } = state.videoPlayer;
+    return {
+        video,
+    }
+}
+export default connect(mapStateToProps, null)(VideoPlayer);
